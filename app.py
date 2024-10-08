0034f1ee1502a5ab50c29ba96c77b401b3970835
Pattanawadee Chawraina <pattanawadee.26.30@gmail.com>
2024-10-09 01:51:30 +0700

    test3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def form():
-    return('hello')
+    return render_template('form.html')
 @app.route('/data')
 def data():
     return render_template('form.html')
